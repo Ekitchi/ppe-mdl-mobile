6fae52f44eb0f0eadd860510ef714cc40e53bd4f
Metalovichinkov <smail.darani@gmail.com>
2015-05-29 00:59:16 +0200

    Indentation remaker
    Parce que honnêtement, déjà qu'Angular c'est illisible, là c'était
    Bagdad. Et vazy que je te colle 2 espaces, puis ici 3 tabulations, là
    bas je met rien, puis vtff je met 1 espace et une tabulation... C'est
    p'tetre pas à votre convenance, mais au moins, c'est uniforme !

--- www/js/app.js
+++ www/js/app.js
@@ -6,85 +6,98 @@
 // 'mdl.controllers' is found in controllers.js
 angular.module('mdl', ['ionic', 'mdl.controllers', 'mdl.service'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+.run(function($ionicPlatform) 
+{
+	$ionicPlatform.ready(function() 
+	{
+	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    	if (window.cordova && window.cordova.plugins.Keyboard) 
+    		{cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);}
+
+    	if (window.StatusBar) 
+    	{// org.apache.cordova.statusbar required
+      		StatusBar.styleDefault();
+    	}
+  	});
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function($stateProvider, $urlRouterProvider) 
+{
+	$stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+	.state('app', 
+	{
+    	url: "/app",
+    	abstract: true,
+    	templateUrl: "templates/menu.html",
+    	controller: 'AppCtrl'
+  	})
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
+	.state('app.search', 
+	{
+    	url: "/search",
+    	views: 
+    	{'menuContent': 
+    		{templateUrl: "templates/search.html"}
+    	}
+  	})
 
-  .state('app.home', {
-    url: "/home",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/home.html"
-      }
-    }
-  })
+	.state('app.home', 
+	{
+    	url: "/home",
+    	views: 
+    	{'menuContent': 
+    		{templateUrl: "templates/home.html"}
+    	}
+  	})
 
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-  .state('app.ligues', {
-      url: "/ligues",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/ligues.html",
-          controller: 'LiguesCtrl'
-        }
-      }
+	.state('app.browse', 
+	{
+    	url: "/browse",
+    	views: 
+    	{'menuContent': 
+    		{templateUrl: "templates/browse.html"}
+    	}
+  	})
+
+	.state('app.ligues', 
+	{
+    	url: "/ligues",
+    	views: 
+    	{'menuContent': 
+        	{
+        		templateUrl: "templates/ligues.html",
+        		controller: 'LiguesCtrl'
+        	}
+    	}
     })
-  .state('app.profil', {
-    url: "/profil/:profilId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/profil.html",
-        controller: 'ProfilCtrl'
-      }
-    }
-  })
+
+	.state('app.profil', 
+	{
+		url: "/profil/:profilId",
+		views: 
+		{'menuContent': 
+			{
+				templateUrl: "templates/profil.html",
+				controller: 'ProfilCtrl'
+			}
+		}
+	})
 
 
-  .state('app.single', {
-    url: "/ligue/:ligueId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/ligue.html",
-        controller: 'LigueCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
+	.state('app.single', 
+	{
+		url: "/ligue/:ligueId",
+		views: 
+		{'menuContent': 
+			{
+				templateUrl: "templates/ligue.html",
+				controller: 'LigueCtrl'
+			}
+		}
+	});
+	// if none of the above states are matched, use this as the fallback
+	
+	$urlRouterProvider.otherwise('/app/home');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,107 +1,129 @@
 angular.module('mdl.controllers', ['mdl.service', 'ngCookies'])
+.service('cookieService', function()
+{
+  var logged;
+  return 
+  {
+    getLoggedStatus: function()
+    {
+    return logged;
+    },
 
-.service('cookieService', function(){
-    var logged;
-    return {
-        getLoggedStatus: function(){
-            return logged;
-        },
-        setLoggedStatus: function(status){
-            logged = status;
-        }
+    setLoggedStatus: function(status)
+    {
+      logged = status;
     }
+  }
 })
 
-.controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', 'MdlService', '$cookieStore', 'cookieService', '$location',  function($scope, $ionicModal, $timeout, MdlService, $cookieStore, cookieService, $location) {
+.controller('AppCtrl', ['$scope', '$ionicModal', '$timeout', 'MdlService', '$cookieStore', 'cookieService', '$location',  function($scope, $ionicModal, $timeout, MdlService, $cookieStore, cookieService, $location) 
+{
   // Form data for the login modal
   $scope.loginData = {};
 
   // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  $ionicModal.fromTemplateUrl('templates/login.html', {scope: $scope})
+    .then(function(modal) 
+    {
+      $scope.modal = modal;
+    });
 
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.closeLogin = function() 
+  {
     $scope.modal.hide();
   };
 
   // Open the login modal
-  $scope.login = function() {
+  $scope.login = function() 
+  {
     $scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function()
   {
-  MdlService.login($scope.loginData.username, $scope.loginData.password)
-  .then(function success(success){
-    console.log(success);
-    if (success.code == 200) {
+    MdlService.login($scope.loginData.username, 
+    $scope.loginData.password).then(function success(success)
+    {
+      console.log(success);
+      if (success.code == 200) 
+      {
+      // Création des cookies
+      $cookieStore.put("Token", success.token.token);
+      $cookieStore.put("User", success.token.user);
 
-    // Création des cookies
-    $cookieStore.put("Token", success.token.token);
-    $cookieStore.put("User", success.token.user);
-    console.log($cookieStore.get("Token"));
-    console.log($cookieStore.get("User"));
-    $scope.logged = cookieService.setLoggedStatus(true);
-    // On redirige vers l'accueil et on recharge pour prendre en compte les cookies fraichement créés.
-    $location.path('/');
-    location.reload();
-    }
-  },
-  function error(error){
-    console.log("ERROR \n");
-    console.log(error);
-  });
+      console.log($cookieStore.get("Token"));
+      console.log($cookieStore.get("User"));
+
+      $scope.logged = cookieService.setLoggedStatus(true);
+      // On redirige vers l'accueil et on recharge pour prendre en compte les cookies fraichement créés.
+      $location.path('/');
+      location.reload();
+      }
+    },
+
+    function error(error)
+    {
+      console.log("ERROR \n");
+      console.log(error);
+    });
   };
-}])
+}])  // .controller END
 
-.controller('LiguesCtrl', ['$scope', 'MdlService', function($scope, MdlService) {
+.controller('LiguesCtrl', ['$scope', 'MdlService', function($scope, MdlService) 
+{
+  MdlService.getLeagueList().then(function success(success) 
+  {
+    console.log(success);
+    $scope.ligues = success.leagues;
+  },
 
-        MdlService.getLeagueList().then(function success(success) {
-        console.log(success);
-        $scope.ligues = success.leagues;
-      }, function error(err){
-        console.log(err);
-      });
-}])
+  function error(err)
+  {
+    console.log(err);
+  });
+}]) // .controller END
 
-.controller('ProfilCtrl', ['$scope', '$stateParams', 'MdlService', '$cookieStore', function($scope, $stateParams, MdlService, $cookieStore) {
-    var idProfil = $stateParams.profilId;
+.controller('ProfilCtrl', ['$scope', '$stateParams', 'MdlService', '$cookieStore', function($scope, $stateParams, MdlService, $cookieStore) 
+{
+  var idProfil = $stateParams.profilId;
 
-    console.log($stateParams);
-    if(idProfil == "self")
-        $scope.profil = $cookieStore.get("User");
-    else {
-        MdlService.getUser(idProfil).then( function success(success) {
-            console.log(success);
-            $scope.profil = success.token.user;
-        },
-        function error(err){
-            console.log(error);
-        });
-    }
-}])
+  console.log($stateParams);
+  if(idProfil == "self")
+    {$scope.profil = $cookieStore.get("User");}
+  else 
+  {
+    MdlService.getUser(idProfil).then( function success(success) 
+    {
+      console.log(success);
+        $scope.profil = success.token.user;
+    },
 
-.controller('LigueCtrl', ['$scope', '$stateParams', 'MdlService', function($scope, $stateParams, MdlService) {
-          var idLeague = $stateParams.ligueId;
+    function error(err)
+    {
+      console.log(error);
+    });
+  }
+}]) // .controller END
 
-          console.log($stateParams);
+.controller('LigueCtrl', ['$scope', '$stateParams', 'MdlService', function($scope, $stateParams, MdlService) 
+{
+  var idLeague = $stateParams.ligueId;
+  console.log($stateParams);
 
-          MdlService.getLeague(idLeague).then( function success(success) {
-            console.log(success);
-                  $scope.leagueName = success.league.name;
-                  $scope.leaguePresident = success.league.president.name;
-                  $scope.leagueEmail = success.league.email;
-                  $scope.leaguePhoneNumber = success.league.phone_number;
-                  $scope.leagueDescription = success.league.description;
+  MdlService.getLeague(idLeague).then( function success(success) 
+  {
+    console.log(success);
+    $scope.leagueName = success.league.name;
+    $scope.leaguePresident = success.league.president.name;
+    $scope.leagueEmail = success.league.email;
+    $scope.leaguePhoneNumber = success.league.phone_number;
+    $scope.leagueDescription = success.league.description;
+  }
 
-              },function error(err){
-                  console.log(error);
-              });
-
-
+  ,function error(err)
+  {
+    console.log(error);
+  }); // .controller END
 }]);
