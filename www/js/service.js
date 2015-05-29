0c4fd5f228658454f9583b78d2aefe36332d3aa5
William <yoshi93@gmail.com>
2015-05-29 18:25:37 +0200

    Added commented url var in service to allow quick change for devs.

--- www/js/service.js
+++ www/js/service.js
@@ -1,69 +1,71 @@
 angular.module('mdl.service', [])
-.factory('MdlService', ['$http', '$q',
-                    function($http, $q) {
+    .factory('MdlService', ['$http', '$q',
+        function($http, $q) {
 
-    // URL var, to change for dev purposes. Change it when you're pulling from another dev, but specifiy the URL change in the commit.
-    var symfonyUrl = 'http://localhost:8888/PPE1/Symfony/web/app_dev.php';
+            // URL var, to change for dev purposes. Change it when you're pulling from another dev, but specifiy the URL change in the commit.
+            var symfonyUrl = 'http://localhost:8888/PPE1/Symfony/web/app_dev.php' // Localhost de William;
+            //var symfonyUrl = 'http://m2l.erwhann-rouge.fr/web/app_dev.php'; //Route du serveur de Guilhem
+            //var symfonyUrl = 'http://localhost/dev/M2L/web/app_dev.php'; //Localhost de Smaïl
 
-    // This kinda initiate the whole promise thing. Don't touch it.
-    function wrapped$httpPromise(httpCallConfig) 
-    {
-        var deferred = $q.defer();
+            // This kinda initiate the whole promise thing. Don't touch it.
+            function wrapped$httpPromise(httpCallConfig)
+            {
+                var deferred = $q.defer();
 
-        $http(httpCallConfig).
-            success(function(body) 
-            {
-                deferred.resolve(body);
-            }).
-            
-            error(function (data) 
-            {
-                deferred.reject(data);
-            });
+                $http(httpCallConfig).
+                    success(function(body)
+                    {
+                        deferred.resolve(body);
+                    }).
 
-        return deferred.promise;
-    };
+                    error(function (data)
+                    {
+                        deferred.reject(data);
+                    });
 
-    //Here are your multiple requests that will establish communication with the REST service.
-    return {
-        login : function(login, password)
-        {
-            return wrapped$httpPromise(
-            {
-                method: 'GET',
-                headers: {'Content-Type': "application/x-www-form-urlencoded"},
-                url: symfonyUrl+'/user/'+login+'/'+password
-            });
-        },
-    
-        getUser : function(user_id)
-        {
-            return wrapped$httpPromise(
-            {
-                method: 'GET',
-                headers: {'Content-Type': "application/x-www-form-urlencoded"},
-                url: symfonyUrl+'/user/'+user_id
-            });
-        },
-    
-        getLeagueList: function()
-        {
-            return wrapped$httpPromise(
-            {
-                method: 'GET',
-                headers: {'Content-Type': "application/x-www-form-urlencoded"},
-                url: symfonyUrl+'/leagues/'
-            });
-        },
-    
-        getLeague: function(league_id)
-        {
-            return wrapped$httpPromise(
-            {
-                method:'GET',
-                headers: {'Content-Type': "application/x-www-form-urlencoded"},
-                url: symfonyUrl+'/league/'+league_id
-            });
-        }
-    } // fin du return
-}]);
+                return deferred.promise;
+            };
+
+            //Here are your multiple requests that will establish communication with the REST service.
+            return {
+                login : function(login, password)
+                {
+                    return wrapped$httpPromise(
+                        {
+                            method: 'GET',
+                            headers: {'Content-Type': "application/x-www-form-urlencoded"},
+                            url: symfonyUrl+'/user/'+login+'/'+password
+                        });
+                },
+
+                getUser : function(user_id)
+                {
+                    return wrapped$httpPromise(
+                        {
+                            method: 'GET',
+                            headers: {'Content-Type': "application/x-www-form-urlencoded"},
+                            url: symfonyUrl+'/user/'+user_id
+                        });
+                },
+
+                getLeagueList: function()
+                {
+                    return wrapped$httpPromise(
+                        {
+                            method: 'GET',
+                            headers: {'Content-Type': "application/x-www-form-urlencoded"},
+                            url: symfonyUrl+'/leagues/'
+                        });
+                },
+
+                getLeague: function(league_id)
+                {
+                    return wrapped$httpPromise(
+                        {
+                            method:'GET',
+                            headers: {'Content-Type': "application/x-www-form-urlencoded"},
+                            url: symfonyUrl+'/league/'+league_id
+                        });
+                }
+            } // fin du return
+        }]);
